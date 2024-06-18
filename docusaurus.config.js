7952ecbe8d557f97bcd9a3ed2952090500ceb6b5
Ashwin Prasad <ashwin@onpar.in>
2024-06-19 01:40:12 +0530

    chore: updated config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,116 +3,119 @@
 
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-const remarkMath = require("remark-math");
-const rehypeKatex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: "Ordinox Docs",
-  tagline: "Bringing DeFi liquidity to BTC",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/ordinox.png",
+const config = async () => {
+  const remarkMath = await import("remark-math");
+  const rehypeKatex = await import("rehype-katex");
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  return {
+    title: "Ordinox Docs",
+    tagline: "Bringing DeFi liquidity to BTC",
+    url: "https://your-docusaurus-test-site.com",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/ordinox.png",
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "facebook", // Usually your GitHub org/user name.
+    projectName: "docusaurus", // Usually your repo name.
 
-  presets: [
-    [
-      "classic",
-      {
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/hwnprsd/ordinox-docs/tree/main/",
-          remarkPlugins: [remarkMath],
-          rehypePlugins: [rehypeKatex],
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+      defaultLocale: "en",
+      locales: ["en"],
+    },
+
+    presets: [
+      [
+        "classic",
+        {
+          docs: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            // Please change this to your repo.
+            // Remove this to remove the "edit this page" links.
+            editUrl: "https://github.com/hwnprsd/ordinox-docs/tree/main/",
+            remarkPlugins: [remarkMath.default],
+            rehypePlugins: [rehypeKatex.default],
+          },
+          blog: {
+            showReadingTime: true,
+            // Please change this to your repo.
+            // Remove this to remove the "edit this page" links.
+            editUrl: "https://github.com/hwnprsd/ordinox-docs/tree/main/",
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/hwnprsd/ordinox-docs/tree/main/",
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      },
+      ],
     ],
-  ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: "Ordinox",
-        logo: {
-          alt: "Ordinox",
-          src: "img/ordinox.png",
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        navbar: {
+          title: "Ordinox",
+          logo: {
+            alt: "Ordinox",
+            src: "img/ordinox.png",
+          },
+          items: [
+            {
+              href: "https://origins.ordinox.xyz",
+              position: "left",
+              label: "Origins Program",
+            },
+            {
+              href: "https://github.com/ordinox",
+              label: "GitHub",
+              position: "right",
+            },
+          ],
         },
-        items: [
-          {
-            href: "https://origins.ordinox.xyz",
-            position: "left",
-            label: "Origins Program",
-          },
-          {
-            href: "https://github.com/ordinox",
-            label: "GitHub",
-            position: "right",
-          },
-        ],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Discord",
-                href: "https://discord.gg/PG8w4RG5jJ",
-              },
-              {
-                label: "Twitter",
-                href: "https://x.com/OrdinoxLabs",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Website",
-                href: "https://ordinox.xyz",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/ordinox",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Ordinox Labs. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        footer: {
+          style: "dark",
+          links: [
+            {
+              title: "Community",
+              items: [
+                {
+                  label: "Discord",
+                  href: "https://discord.gg/PG8w4RG5jJ",
+                },
+                {
+                  label: "Twitter",
+                  href: "https://x.com/OrdinoxLabs",
+                },
+              ],
+            },
+            {
+              title: "More",
+              items: [
+                {
+                  label: "Website",
+                  href: "https://ordinox.xyz",
+                },
+                {
+                  label: "GitHub",
+                  href: "https://github.com/ordinox",
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} Ordinox Labs. Built with Docusaurus.`,
+        },
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
+        },
+      }),
+  };
 };
 
-module.exports = config;
+module.exports = config();
